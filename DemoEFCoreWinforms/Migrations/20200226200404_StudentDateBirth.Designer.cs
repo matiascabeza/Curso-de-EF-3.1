cd0632aa7c71f7e4c03bfb0c8968b5284fe879e7
matiascabeza <matiascabeza2012@gmail.com>
2020-02-26 17:08:23 -0300

    Se agrega el atributo DateBirth en la clase Student, se realiza Add-Migration para generar la migracion y Update-Database que actualiza la tabla student agregando la columna DataBirth

--- DemoEFCoreWinforms/Migrations/20200226200404_StudentDateBirth.Designer.cs
+++ DemoEFCoreWinforms/Migrations/20200226200404_StudentDateBirth.Designer.cs
@@ -4,14 +4,16 @@ using DemoEFCoreWinforms.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DemoEFCoreWinforms.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200226200404_StudentDateBirth")]
+    partial class StudentDateBirth
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- DemoEFCoreWinforms/Models/Student.cs
+++ DemoEFCoreWinforms/Models/Student.cs
@@ -10,5 +10,6 @@ namespace DemoEFCoreWinforms.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public DateTime DateBirth { get; set; }
     }
 }
