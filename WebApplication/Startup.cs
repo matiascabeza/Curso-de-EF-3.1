056a66287eb4b4ecdbbaa9c3b1e904f2899a63fa
matiascabeza <matiascabeza2012@gmail.com>
2020-02-26 13:47:03 -0300

    Se crea un proyecto asp.net web mvc y se agrega el EF Core 3.1.0. Se pasa por contructor la configuracion de la conexion de DbContextOption y se configura en el startup de asp.net y se agrega en el servicio de proveedor json la conexion de la base de datos

---
+++ WebApplication/Startup.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Configuration saca informacion de Proveedor de configuracion
+            services.AddDbContext<ApplicationDbContext>(options => 
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+            .EnableSensitiveDataLogging(true)
+            .UseLoggerFactory(Logger)
+            );
+            services.AddControllersWithViews();
+        }
+
+        public static readonly ILoggerFactory Logger = LoggerFactory.Create(builder =>
+        {
+            builder
+                .AddFilter((catagory, level) => 
+                catagory == DbLoggerCategory.Database.Command.Name 
+                && level == LogLevel.Information)
+                .AddConsole();
+        });
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
