4a77959c8171ecea5dce68f8b7d265c5cc987028
matiascabeza <matiascabeza2012@gmail.com>
2020-03-03 01:31:40 -0300

    Se agrega el metodo de insertregistro que utiliza el dbcontext para insertar un registro en la base de datos

---
+++ EFCore_Mod3/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoEFCoreWinforms.Models
+{
+    class ApplicationDbContext : DbContext
+    {
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Data Source=DESKTOP-75L2R7E\\SQLEXPRESS;Initial Catalog=DemoWinForms2;Integrated Security=True")
+				.EnableSensitiveDataLogging(true)
+				.UseLoggerFactory(MyLoggerFactory);
+		}
+
+		public static readonly ILoggerFactory MyLoggerFactory = LoggerFactory.Create(builder =>
+		{
+			builder
+			   .AddFilter((category, level) =>
+				   category == DbLoggerCategory.Database.Command.Name
+				   && level == LogLevel.Information).AddConsole();
+		});
+		
+		public DbSet<Student> Students { get; set; }
+		
+	}
+}
