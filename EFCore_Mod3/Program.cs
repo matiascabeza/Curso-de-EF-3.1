0ff83be7b9bef8be19bbc1c92679da5bee265ad1
matiascabeza <matiascabeza2012@gmail.com>
2020-03-03 01:41:07 -0300

    Se crea la insercion multiple de los registros en la tabla, se vera que cuando se inserta mas de 10 registro se crea una tabla temporal que despues inserta los datos

--- EFCore_Mod3/Program.cs
+++ EFCore_Mod3/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DemoEFCoreWinforms.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,56 @@ namespace EFCore_Mod3
     {
         static void Main(string[] args)
         {
+        }
+
+        static void InsertRegistros() 
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var student1 = new Student();
+                // paso 1: Creamos el Objecto
+                student1.Name = "Felipe Gavilán";
+                student1.DateBirth = new DateTime(1934, 7, 20);
+
+                // paso 2: Notificamos que queremos agregar un estudiante
+                context.Students.Add(student1);
+
+                // paso 3: Guardamos los mitos
+                context.SaveChanges();
+            }
+        }
+
+        static void InsertMultipleRegistros()
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                // paso 1: Creamos el Objecto
+                var student1 = new Student();
+                student1.Name = "Felipe Gavilán";
+                student1.DateBirth = new DateTime(1934, 7, 20); 
+                var student2 = new Student();
+                student1.Name = "Matias Cabeza";
+                student1.DateBirth = new DateTime(1988, 5, 21);
+
+                IList<Student> list1 = new List<Student>() { student1, student2 };
+                IList<Student> list2 = new List<Student>();
+                for(int i = 0; i < 10; i++)
+                {
+                    list2.Add(new Student()
+                    {
+                        Name = "Estudiante " + i.ToString(),
+                        DateBirth = new DateTime(1900 + i, 1, 2)
+                    });
+                }
+                // paso 2: Notificamos que queremos agregar un estudiante
+                context.Students.AddRange(list1);
+                context.Students.AddRange(list2);
+
+                // paso 3: Guardamos los mitos
+                context.SaveChanges();
+            }
+
+
         }
     }
 }
