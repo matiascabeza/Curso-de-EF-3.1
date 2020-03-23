580e18ef26d63c43e3e3f8ed526ee8d336e119a1
matiascabeza <matiascabeza2012@gmail.com>
2020-03-23 16:21:42 -0300

    Se agrega la ejecucion diferida, como realizar consultas con asqueryable.

--- EFCore_Mod3/Program.cs
+++ EFCore_Mod3/Program.cs
@@ -4,12 +4,13 @@ using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Transactions;
 
 namespace EFCore_Mod3
 {
     class Program
     {
-        static void Main(string[] args) { FromSqlInterpolated(); }
+        static void Main(string[] args) { TransationEnvironment(); }
 
         static void InsertRegistros()
         {
@@ -241,6 +242,72 @@ namespace EFCore_Mod3
                 var report1 = context.Students
                     .FromSqlInterpolated($"SELECT * FROM Students WHERE Id = {Id}")
                     .FirstOrDefault();
+            }
+        }
+
+        static void TransationCommon()
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                using (var trasaction = context.Database.BeginTransaction())
+                {
+                    var student1 = new Student();
+                    student1.Name = "Felipe1 Fato";
+                    student1.DateBirth = new DateTime(1934, 7, 20);
+                    context.Students.Add(student1);
+                    context.SaveChanges();
+                    // El Id tendra un valor valido
+                    Console.WriteLine("Id del estudiante" + student1.Id);
+                    //vamos a revertir la operacion realizada
+                    trasaction.Commit();
+                }
+                    
+            }
+        }
+
+        static void TransationEnvironment()
+        {
+            //Verificar difencia entre los using de TransactionScope
+            //Transacion de ambiente que ocurren por hilo
+            using (var scope = new TransactionScope())
+            {
+                using (var context = new ApplicationDbContext())
+                {
+
+                    var student1 = new Student();
+                    student1.Name = "Transaction Scope 1";
+                    context.Students.Add(student1);
+                    context.SaveChanges();
+                }
+
+                using (var context = new ApplicationDbContext())
+                {
+
+                    var student2 = new Student();
+                    student2.Name = "Transaction Scope 2";
+                    context.Students.Add(student2);
+                    context.SaveChanges();
+                }
+                //Descomenta la siguiente linea si quiere que se persista en la base de datos
+                //scope.Complete();
+            }
+        }
+        //Ejecucion Diferida
+        static void DeferredExecution()
+        {
+            //Reporte de registros con GroupBy
+            using (var context = new ApplicationDbContext())
+            {
+                //Forma 1: Funciones en una linea 
+                var report1 = context.Students
+                    .Where(x => x.DateBirth.Year < 1990)
+                    .OrderByDescending(x => x.DateBirth).ToList();
+
+                //Forma 2: Funciones en una linea
+                var query = context.Students.AsQueryable();
+                query = query.Where(x => x.DateBirth.Year < 1990);
+                query = query.OrderByDescending(x => x.DateBirth);
+                var report2 = query.ToList();
             }
         }
     }
